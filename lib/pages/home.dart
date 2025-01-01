2cbac875beb2abd1bfaead316e7c5dcbf8329345
Aron <aaronkipkirui015@gmail.com>
2025-01-01 23:49:29 +0300

    worked on searchbar just below the appBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,26 +7,84 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Breakfast',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-          fontWeight: FontWeight.bold
-        ),
-        
+      appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff1D1617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0
+                )
+              ]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: EdgeInsets.all(15),
+                prefixIcon: SvgPicture.asset('asset/icons/Search.svg'),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none
+                )
+              ),
+            ),
+          )
+        ],
       ),
-        centerTitle: true,
-        elevation: 0.0,
-        backgroundColor: Colors.white,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(10)
-          ),
-        ),
-        ),
     );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text('Breakfast',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.bold
+      ),
+      
+    ),
+      centerTitle: true,
+      elevation: 0.0,
+      backgroundColor: Colors.white,
+      leading: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: SvgPicture.asset(
+          'assets/icons/Arrow - Left 2.svg',
+          height: 20,
+          width: 20),
+      ),
+
+      actions: [
+        GestureDetector(
+          onTap: () => {
+            
+          },
+        child: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        width: 37,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: SvgPicture.asset(
+          'assets/icons/dots.svg',
+          height: 20,
+          width: 20),
+      ),
+        ),
+      ],
+      );
   }
 }
